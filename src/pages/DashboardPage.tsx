03adc9442ec0411b534c664873cb15fb1110a5cb
meta-crawler <johncarlosoft@gmail.com>
2023-07-01 21:51:00 +0800

    feat: update in first info section in dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import ComingSoonPage from 'src/pages/ComingSoonPage';
+import { StatusCard, StatusTable } from 'src/sections/dashboard-section';
+import { OperationStatus } from 'src/lib/constants/status';
 
 export default function DashboardPage() {
   return (
-    <div className="w-full h-full flex flex-row p-3">
-      <ComingSoonPage />
+    <div className="w-full h-full flex flex-col gap-3">
+      <div className="h-22 flex flex-col lg:flex-row gap-3">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-3">
+          <StatusCard status={OperationStatus.NORMAL} title={`System ${1} Status`} />
+          <StatusCard status={OperationStatus.NORMAL} title="Temperature Status" />
+          <StatusCard status={OperationStatus.WARNING} title="Over Charge Monitoring" />
+          <StatusCard status={OperationStatus.NORMAL} title={`System ${1} Status`} />
+        </div>
+        <StatusTable />
+      </div>
     </div>
   );
 }
---
+++ src/sections/dashboard-section/StatusCard.tsx
@@ -0,0 +1,46 @@
+import React, { useMemo } from 'react';
+import { shadows as customShadows } from 'src/theme/shadows';
+import typography from 'src/theme/typography';
+import { CARD } from 'src/config-global';
+import { OperationStatus } from 'src/lib/constants/status';
+
+type IStatusCardProps = {
+  title: string;
+  status: string;
+};
+
+export default function StatusCard({ title, status }: IStatusCardProps) {
+  const shadows = customShadows();
+  const statusColor = useMemo(() => {
+    switch (status) {
+      case OperationStatus.NORMAL:
+        return 'bg-primary-main';
+      case OperationStatus.WARNING:
+        return 'bg-warning-main';
+      case OperationStatus.DANGER:
+        return 'bg-error-main';
+    }
+  }, [status]);
+
+  return (
+    <div
+      className="w-full flex flex-row items-center justify-between gap-x-2"
+      style={{
+        padding: `${CARD.PADDING}px`,
+        boxShadow: shadows[CARD.BOX_SHADOW],
+        borderRadius: `${CARD.BORDER_RADIUS}px`,
+      }}
+    >
+      <p className="flex-1 text-text-primary" style={typography.h6}>
+        {title}
+      </p>
+      <div
+        className={`flex items-center justify-center rounded-full w-20 h-fit py-3 ${statusColor}`}
+      >
+        <p className="uppercase text-center text-white font-black" style={typography.overline}>
+          {status}
+        </p>
+      </div>
+    </div>
+  );
+}
